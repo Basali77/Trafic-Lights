27ea33f79294d1da855840fb978f74137e3fd436
Vishali786 <161928080+Basali77@users.noreply.github.com>
2024-03-19 15:01:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+function clred(){
+    document.getElementById("red")
+    .style.backgroundColor = "red";
+
+document.getElementById("yellow")
+    .style.backgroundColor ="rgba(255,255,0,0.2)";
+
+    document.getElementById("green")
+    .style.backgroundColor ="rgba(0,255,0,0.2)" ;
+}
+function clyellow (){
+    document.getElementById("yellow")
+    .style.backgroundColor = "yellow";
+
+    document.getElementById("red")
+    .style.backgroundColor = "rgba(255,0,0,0.2)";
+
+    document.getElementById("green")
+    .style.backgroundColor = "rgba(0,255,0,0.2)";
+}
+ function clgreen (){
+    document.getElementById("green")
+    .style.backgroundColor = "lime";
+
+    document.getElementById("red")
+    .style.backgroundColor = "rgba(255,0,0,0.2)";
+
+    document.getElementById("yellow")
+    .style.backgroundColor = "rgba(255,255,0,0.2)";
+ }
+ function clall (){
+    document.getElementById("green")
+    .style.backgroundColor = "lime";
+
+    document.getElementById("red")
+    .style.backgroundColor = "red";
+
+    document.getElementById("yellow")
+    .style.backgroundColor = "yellow";
+ }
